286126a7202e2495319f9c11cf15c0cc6279a686
Artyom <85443778+artyom-pe4yorckin@users.noreply.github.com>
2021-06-14 17:07:49 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="https://api-maps.yandex.ru/2.1/?apikey=7a43dc77-4610-4d0a-b182-327a40810f40&lang=ru_RU&load=package.full" type="text/javascript"></script>
+  <script src="map.js"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <?php
+  $json = json_decode(file_get_contents("offices.json"), true);
+  ?>
+  <div id="map-id" style="width: 100%; height: 400px"></div>
+  <div id="map-data">
+    <div class="country-city">
+      <div class="btn-wrapper">
+        <?php
+        foreach($json as $key=>$value){
+          ?><button data-country="<?=$key?>" data-coords=""><?=$value[0]?></button><?php
+        }
+        ?>
+      </div>
+      <div class="city-wrapper"><?php
+        foreach($json as $key=>$value){
+          ?><div class="city-list <?=$key?>"><?php 
+          for($i=1; $i<count($value); $i++){
+            ?><details>
+              <summary><?=$value[$i]["city"]?></summary><?php 
+              $offices = $value[$i]["offices"];
+              foreach($offices as $f){?>
+                <div class="office">
+                  <p class="addres"><?=$f["addres"]?></p>
+                  <p><?=$f["name"]?></p>
+                  <div class="phone-wrapper">
+                    <?php foreach($f["phone"] as $p){
+                      ?><p><?=$p?></p><?php
+                    }?>
+                  </div>
+                  <div class="email-wrapper">
+                    <?php foreach($f["email"] as $e){
+                        ?><p><?=$e?></p><?php
+                    }?>
+                  </div>
+                </div>
+              <?php }?>
+            </details><?php
+          }
+          ?>
+        </div><?php
+        }
+      ?></div>
+    </div>
+    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A59ab7d8a84a88707e95dd0331c6fbbdd3f5f0c9eadd88a293893cdfc8fe3af3d&source=constructor" width="100%" height="750" frameborder="0"></iframe>
+  </div>
+</body>
+</html>
---
+++ map.js
@@ -0,0 +1,185 @@
+window.addEventListener("load", ready);
+function ready() {
+  let btnWrapper = document.querySelector(".btn-wrapper");
+  let countries = btnWrapper.querySelectorAll(".btn-wrapper [data-country]");
+  let cityWrapper = document.querySelector(".city-wrapper");
+  let offices = document.querySelectorAll(".city-list details");
+  countries[0].classList.add("active");
+  let defaultCountry = countries[0].dataset.country;
+  document.querySelector(`.city-list.${defaultCountry}`).classList.add("active");
+  for (let c of countries) {
+    c.onclick = showAddresses;
+  }
+  function showAddresses() {
+    btnWrapper.querySelector(".active").classList.remove("active");
+    this.classList.add("active");
+    let country = this.dataset.country;
+    cityWrapper.querySelector(".active").classList.remove("active");
+    cityWrapper.querySelector(`.${country}`).classList.add("active");
+    console.log(this.dataset.country)
+  }
+
+  //Яндекс карта
+  /*
+  myMap.geoObjects.add(myGeoObject) — добавление объекта на карту,
+  myMap.geoObjects.remove(myGeoObject) — удаление объекта с карты.
+  */
+  ymaps.ready(init);
+  function init() {
+    let circleLayout = ymaps.templateLayoutFactory.createClass('<div class="circle-placemark"></div>');
+    var commonContent = ymaps.templateLayoutFactory.createClass('<div class="circle-placemark">$[properties.iconContent]</div>');
+    // Создание карты.
+    var myMap = new ymaps.Map(document.querySelector("#map-id"), {
+      // Координаты центра карты.
+      // Порядок по умолчанию: «широта, долгота».
+      center: [55.76, 37.64],
+      // Уровень масштабирования. Допустимые значения:
+      // от 0 (весь мир) до 19.
+      zoom: 4,
+      mapAutoFocus: false,
+      clusterize: true
+    }),
+      clusterer = new ymaps.Clusterer({
+        //preset: 'islands#invertedNightClusterIcons',
+        /**
+         * Ставим true, если хотим кластеризовать только точки с одинаковыми координатами.
+         */
+        groupByCoordinates: false,
+        /**
+         * Опции кластеров указываем в кластеризаторе с префиксом "cluster".
+         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/ClusterPlacemark.xml
+         */
+        clusterHideIconOnBalloonOpen: false,
+        geoObjectHideIconOnBalloonOpen: false,
+      },
+      {
+        iconLayout: commonContent,//форма кликабельной области placemark
+        iconShape: {
+          type: 'Circle',
+          coordinates: [0, 0],//центр круга
+          radius: 25//радиус
+        }
+      }
+      )
+
+    let myCollection = new ymaps.GeoObjectCollection();
+
+    //массив точек на карте
+    fetch('/offices.json')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        makePoints(data[defaultCountry]);
+        //переключение стран
+        for (let c of countries) {
+          c.onclick = showAddresses;
+        }
+        for (let f of offices) {
+          f.onclick = openBalloon;
+        }
+
+        function showAddresses() {
+          btnWrapper.querySelector(".active").classList.remove("active");
+          this.classList.add("active");
+          let country = this.dataset.country;
+          cityWrapper.querySelector(".active").classList.remove("active");
+          cityWrapper.querySelector(`.${country}`).classList.add("active");
+          //удаление всех точек с карты
+          myMap.geoObjects.removeAll();
+          clusterer.removeAll();
+          //отображение новых точек
+          makePoints(data[country]);
+        }
+      });
+
+    function openBalloon() {
+      let city = this.querySelector("summary").textContent;
+      let addres = this.querySelector(".addres").textContent;
+      let baloonContent = this.querySelector(".office").innerHTML;
+      ymaps.geocode(`${city}, ${addres}`).then(
+        function (res) {
+          let coords = getCitycenter(res.geoObjects.get(0).properties.getAll().boundedBy);
+          myMap.balloon.open(coords, { content: baloonContent }, { closeButton: true });
+        },
+        function (err) {
+          // Обработка ошибки.
+          console.log(err)
+        }
+      );
+    }
+
+    let points = [];
+    function makePoints(arr) {
+      for (let a = 1; a < arr.length; a++) {
+        for (let f of arr[a].offices) {
+          ymaps.geocode(`${arr[a].city}, ${f.addres}`).then(
+            function (res) {
+              let coords = getCitycenter(res.geoObjects.get(0).properties.getAll().boundedBy);
+              let ballooncontent = [
+                f.addres,
+                f.name,
+                f.email,
+                f.phone
+              ];
+              addPlacemark(coords, ballooncontent);
+            },
+            function (err) {
+              // Обработка ошибки.
+              console.log(err)
+            }
+          );
+        }
+      }
+    }
+
+    function getCitycenter(arr) {
+      return [(arr[0][0] + arr[1][0]) / 2, (arr[0][1] + arr[1][1]) / 2]
+    }
+
+    console.log("до")
+    //добавление точек на карту
+    function addPlacemark(coords, content) {
+      function getP(arr) {
+        let result = "";
+        for (let a of arr) {
+          result += `<p>${a}</p>`;
+        }
+        return result;
+      }
+      let placemark = new ymaps.Placemark(
+        coords,
+        {
+          type: "Point",
+          balloonContentBody: `<p class="title">${content[0]}</p>
+        <p>${content[1]}</p>
+        <div class="phone-wrapper">${getP(content[3])}</div>
+        <div class="email-wrapper">${getP(content[2])}</div>`,
+        },
+        {
+          iconLayout: circleLayout,//форма кликабельной области placemark
+          iconShape: {
+            type: 'Circle',
+            coordinates: [0, 0],//центр круга
+            radius: 25//радиус
+          },
+          hideIconOnBalloonOpen: false
+        }
+      );
+      placemark.events.add('click', function (e) {
+        myMap.setCenter(placemark.geometry.getCoordinates());
+      });
+      myCollection.add(placemark);
+      myMap.geoObjects.add(placemark);
+      points.push(placemark);
+      //console.log(coords)
+      clusterer.add(placemark);
+      myMap.geoObjects.add(clusterer);
+      myMap.setBounds(clusterer.getBounds(), {
+        checkZoomRange: true
+      });
+    }
+    console.log("после")
+      
+  }
+}
